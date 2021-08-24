678aef5afee6be89a836bac945faa5cac8945bf5
duc.nguyen <nguyenmanhduc406@gmail.com>
2021-08-24 14:16:03 +0700

    Bai2

--- Bai2MVC/Models/Bai2MVCController.cs
+++ Bai2MVC/Models/Bai2MVCController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Bai2MVC.Models;
 
 namespace Bai2MVC.Models
 {
     public class Bai2MVCController : Controller
     {
         // GET: Bai2MVC
-
+        GiaiPT PTBN = new GiaiPT();
         public ActionResult Index()
         {
             return View();
@@ -18,5 +19,17 @@ namespace Bai2MVC.Models
         {
             return View();
         }
+        public ActionResult GiaiPTB1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GiaiPTB1(double hsA, double hsB)
+        {
+            double X = PTBN.GiaiPTBN(hsA, hsB);
+            ViewBag.nghiemPT = X;
+            return View();
+        }
+
     }
 }
---
+++ Bai2MVC/Models/GiaiPT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bai2MVC.Models
+{
+    public class GiaiPT
+    {
+        public double GiaiPTBN(double a, double b)
+        {
+            double x;
+            x = -b / a;
+            return x;
+        }
+    }
+}
