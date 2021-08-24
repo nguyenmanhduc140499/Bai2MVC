9ae31f801c88a5781ee046091d3af143e2c889b5
duc.nguyen <nguyenmanhduc406@gmail.com>
2021-08-24 13:51:54 +0700

    Add project files.

---
+++ Bai2MVC/Models/Bai2MVCController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai2MVC.Models
+{
+    public class Bai2MVCController : Controller
+    {
+        // GET: Bai2MVC
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hello()
+        {
+            return View();
+        }
+    }
+}
